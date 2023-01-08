d9b4fc4c975fe90cf3f6c25897eedbc3f9763d84
ericduro <eriic11duro@gmail.com>
2023-01-08 12:37:43 +0100

    Añadiendo la llamada AJAX con JQuery

--- js/script_jquery.js
+++ js/script_jquery.js
@@ -15,3 +15,20 @@ $("#formulario01").submit(function(event) {
         $("#label_apellidos").css("color","black");
     }
 });
+
+$("#tamano").on('change', function() {
+  var size = this.value;
+   console.log(size);
+   $.ajax({
+      url: "http://localhost:5000/checksize",
+      type: "POST",
+      data: {
+         tamano: size
+      },
+      success: function(response) {
+         console.log(response);
+         $("#resultado_tamano").html(response);
+      }
+   });
+  });
+  
